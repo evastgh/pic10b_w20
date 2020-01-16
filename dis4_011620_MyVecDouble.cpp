83631fbf257b54bbe23b3f3a3b754c70a9a22f76
Stephanie R. Wang <7941538+evastgh@users.noreply.github.com>
2020-01-16 14:01:47 -0800

    Update dis4_011620_MyVecDouble.cpp

--- dis4_011620_MyVecDouble.cpp
+++ dis4_011620_MyVecDouble.cpp
@@ -1,36 +1,34 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 struct MyVecDouble {
-    MyVecDouble(size_t size_in, double value) : m_size(size_in), m_data(new double[m_size]) {
+    MyVecDouble(size_t size_in, double value)
+        : m_size(size_in)
+        , m_data(new double[m_size]) 
+    {
+        m_data = new double[m_size];
         for (size_t i = 0; i < m_size; ++i)
             m_data[i] = value;
     }
     ~MyVecDouble() { delete[] m_data; }
-    
     void resize(size_t new_size) {
         double* new_data = new double[new_size];
-        for (size_t i = 0; i < new_size && i < m_size; ++i)
-            new_data[i] = m_data[i];
-        delete[] m_data;
+        for (size_t i = 0; i < m_size && i < new_size; ++i) 
+            new_data[i] = m_data[i]; // deep copy
         
-        m_size = new_size;
+        delete[] m_data;
         m_data = new_data;
+        m_size = new_size;
     }
-    
     size_t size() const { return m_size; }
-    double& operator[](size_t i) { return m_data[i]; }
-    
+    double operator[](size_t i) { return m_data[i]; }
     private:
     size_t m_size;
     double* m_data;
 };
-
 int main()
 {
-    vector<double> v(20, 55.66);
-    // MyVecDouble v(20, 55.66);
+    MyVecDouble v(20, 55.66);
     v.resize(10);
     for (size_t i = 0; i < v.size(); ++i)
         cout << v[i] << ' ';
