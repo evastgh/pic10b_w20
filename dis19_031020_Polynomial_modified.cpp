19f6d474d2d9159feebddff686c3a771701e6204
Stephanie R. Wang <7941538+evastgh@users.noreply.github.com>
2020-03-10 13:51:32 -0700

    Update dis19_031020_Polynomial_modified.cpp

--- dis19_031020_Polynomial_modified.cpp
+++ dis19_031020_Polynomial_modified.cpp
@@ -7,8 +7,11 @@ class Polynomial {
 	vector<double> coeff;
 
 public:
-	Polynomial() : coeff(1, 0.0) {}
-	Polynomial(const vector<double>& coeff_in) : coeff(coeff_in) {}
+	Polynomial() : coeff(1, 0.0) {} // this is a polynomial p = 0.0 + 0.0x + ...
+	Polynomial(const vector<double>& coeff_in) : coeff(coeff_in) {
+	    if (coeff_in.size() == 0)
+	        coeff.push_back(0.0);
+	}
 	size_t get_deg() const { return coeff.size() - 1; }
 	bool operator<(const Polynomial& other) {
 		// this allows (p1 < p2)
@@ -33,6 +36,8 @@ public:
 	bool operator>=(const Polynomial& other) { return !(*this < other); }
 	Polynomial& operator=(const Polynomial& other) { this->coeff = other.coeff; return *this; }
 	Polynomial& operator+=(const Polynomial& other) {
+	    // Calling 'p1 += p2' will modify the state of p1; p2 is unchanged
+	    // While executing the operator+=, '*this' refers to p1 
 		if (this->get_deg() >= other.get_deg()) {
 			for (size_t i = 0; i <= other.get_deg(); ++i)
 				coeff[i] += other.coeff[i];
@@ -61,22 +66,52 @@ public:
 		return *this;
 	}
 
+    // This allows 'cout << (-p1) << endl; '
+    // This also allows 'p1 += (-p2); '
+    // 
+    // Recall: prefix operator is 'T& operator++()'
+    //        postfix operator is 'T operator++(int)'
+    // Calling for '-p1', it's prefix
 	Polynomial operator-() {
-		Polynomial temp;
+		Polynomial temp; // construct a zero polynomial
 		temp -= *this;
 		return temp;
 	}
-	operator vector<double>() const { return coeff; }
+	
+	// 'vector<double> vec' allows 'vec[1]'
+	// Since Polynomial is just a vector, can we do something similar?
+	// This following operators allow calling for 'Polynomial p1' with 'p1[0]'
+	// This allows ' p1[0] = 5566; '
 	double& operator[] (size_t i) { return coeff[i]; }
-	double operator[] (size_t i) const { return coeff[i]; } // what happens if I forgot const?
+    double operator[] (size_t i) const { return coeff[i]; } // TEST THIS: what happens if I forgot const?
+	
+	// Note that this operator is not declared as 
+	// double& operator(size_t i) { return coeff[i]; }
+	// This allows ' p1(0) ', works similarly as ' p1[0] '
 	double& operator() (size_t i) { return coeff[i]; }
 	double operator() (size_t i) const { return coeff[i]; }
 
+    // This is a type-casting operator
+    // Casting a ' double d = 5.6; ' to int is ' int n = int(d); '
+    // It allows calls for ' Polynomial p; ' with ' vector<double> vec = vector<double>(p); '
+    // It's somewhat like the reverse of the constructor Polynomial(const vector<double>& c), 
+    // which turns a vector<double> to a Polynomial
+    operator vector<double>() const {
+        return coeff;
+        // The return value of this functional call is of type 'vector<double>'
+        // The above is actualyl the same as doing:
+        // vectour<double> temp = coeff;
+        // return temp;
+    }
+	
 	friend ostream& operator<<(ostream& os, const Polynomial& poly);
 	friend istream& operator>>(istream& is, Polynomial& poly);
 
 	friend double& constant_term(Polynomial& poly);
 	friend double& leading_term(Polynomial& poly);
+	
+	double& constant_term_inclass(Polynomial& poly) { return poly.coeff[0]; }
+	double& constant_term_inclass_noinput() { return coeff[0]; }
 
 	friend Polynomial operator+(const Polynomial& p1, const Polynomial& p2);
 
@@ -112,7 +147,10 @@ istream& operator>>(istream& is, Polynomial& poly) {
 }
 
 Polynomial operator+(const Polynomial& p1, const Polynomial& p2) {
-	Polynomial temp(p1.coeff);
+    // I didn't define a constructor Polynomial(const Polynomial& other)
+    // But, I did have constructor Polynomial(const vector<double>& coeff)
+    // And the type-casting operator operator vector<double>(const Polynomial& poly)
+	Polynomial temp(p1);
 	temp += p2;
 	return temp;
 }
@@ -126,18 +164,40 @@ int main() {
 	cout << "p1 = " << p1 << endl;
 	cout << "p2 = " << p2 << endl;
 
+	/* 
 	p1 += vector<double>({ 1.0 , 2.0 });
 	cout << "p1 = " << p1 << endl;
-
+    
+    
 	leading_term(p1) = 5566;
-	cout << "p1 = " << p1 << endl;
-
+	cout << "p1 = " << p1 << endl; 
+	
+	p1.constant_term_inclass(p1) = 101;
+	p1.constant_term_inclass_noinput() = 105;
+    */
+    /*
 	cout << "-p2 = " << (-p2) << endl;
+	cout << "p2(0) = " << p2(0) << endl;
+	p2[0] = 5566;
+	cout << "p2 = " << p2 << endl;
+	
 
-	vector<double> coeff_copy = p2;
-	cout << "coeff_copy = " << coeff_copy[0] << endl;
+	vector<double> coeff_copy = vector<double>(p2);
+	cout << "coeff_copy.size() = " << coeff_copy.size() << endl;
+	
+	// Note the implicit casting:
+	vector<double> coeff_copy_p1 = p1;
+	Polynomial p3(coeff_copy_p1);
+	cout << "p3 = " << p3 << endl;
+    */
 
 	cout << "p1 + p2 = " << (p1 + p2) << endl;
+	cout << "p1 = " << p1 << endl;
+	cout << "p2 = " << p2 << endl;
+	
+	vector<double> vec = { 33.4, 5.8, 3.14 };
+	cout << "p2 + vec = " << (p2 + vec) << endl;
+
 
 	// cout << "Is p2 < p1? Answer: " << (p2 < p1) << endl;
 
